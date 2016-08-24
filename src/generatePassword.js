bed32a8fea63f5e0769ce92c78887c921549e641
badavis <bdavi015@ucr.edu>
2016-08-23 21:54:04 -0700

    Test generatePassword()

--- src/generatePassword.js
+++ src/generatePassword.js
@@ -1,14 +1,28 @@
 import crypto from 'crypto';
 import _ from 'lodash';
+import Debug from 'debug';
+const debug = new Debug('src:PasswordGenerator:');
 
 class PasswordGenerator {
   constructor() {}
 
-  generateRandomNumber(maximumValue) {
-  	if(maximumValue === undefined) maximumValue = 10;
-  	//var rand = Math.floor(Math.random() * (maximumValue));
-    return crypto.randomBytes(256)[0] % maximumValue;
-  	//return rand;
+  generateRandomNumber(maximumValue = 10, minimumValue = 0, retry = 0) {
+    debug('Input values to number generator... Max: ', maximumValue, 'Min: ', minimumValue, 'retry: ', retry);
+    if(retry >= 1000) { // if number generation fails 1000 times in a row throw error
+      throw new Error('Password Generation Failed. Max 1000 attempts exceeded');
+    }
+    if(minimumValue > maximumValue){
+      throw new Error('minimumValue cannot be greater than maximumValue');
+    }
+    if(minimumValue === maximumValue) return maximumValue;
+
+    const rand = crypto.randomBytes(256)[0] % maximumValue;
+    if((rand < minimumValue) && ((rand + minimumValue) >= maximumValue)) { // if true, rand does not meet given criteria and must be regenerated
+      return this.generateRandomNumber(maximumValue, minimumValue, retry++);
+    }
+    else if(rand < minimumValue) return rand + minimumValue;
+
+    return rand;
   }
 
   generateSpecialCharacter() {
@@ -26,31 +40,34 @@ class PasswordGenerator {
   }
 
   generatePassword(options) {
+    if(options === undefined) options = {};
+    if(options.minimumLength < 4) throw new Error('Error: minimumLength must be greater than or equal to 4');
+    if(!options.hasOwnProperty('upperCaseAlpha')) options.upperCaseAlpha = true; // default
+    if(!options.hasOwnProperty('lowerCaseAlpha')) options.lowerCaseAlpha = true; // default
+    if(!options.hasOwnProperty('specialCharacter')) options.specialCharacter = true; // default
+    if(!options.hasOwnProperty('number')) options.number = true; // default
+    if(!options.hasOwnProperty('minimumLength')) options.minimumLength = 8; // default
+    if(!options.hasOwnProperty('maximumLength')) options.maximumLength = options.minimumLength + 4; // default
+    if(!options.hasOwnProperty('exactLength')) {  // if no exactLength is specified, password length will be between minimiumLength and maximumLength (inclusive)
+      debug('Password length generation result: ');
+      options.exactLength = this.generateRandomNumber(options.maximumLength + 1, options.minimumLength); // add one to maximumLength to ensure inclusivity
+      debug(options.exactLength);
+    }
+
     let password = [];
-    let functionArray = [this.generateSpecialCharacter, this.generateRandomNumber, this.generateLowerCaseAlpha, this.generateUpperCaseAlpha];
-
-    const requirements = {
-      lowercase: this.generateLowerCaseAlpha(),
-      uppercase: this.generateUpperCaseAlpha(),
-      number: this.generateRandomNumber(10),
-      specialChar: this.generateSpecialCharacter()
-    }
-
-    let passwordLength;
-    options.hasOwnProperty('exactLength') ? passwordLength = options.exactLength : passwordLength = 8; // default
-
-    if (!options.hasOwnProperty('minimumLength')) {
-      options.minimumLength = 8;
-    }
-
-    if (!options.hasOwnProperty('maximumLength')) {
-      options.maximumLength = 26;
-    }
+    const requirements = {}
+    options.upperCaseAlpha ? requirements.upperCaseAlpha = this.generateUpperCaseAlpha() : requirements.upperCaseAlpha = false;
+    options.lowerCaseAlpha ? requirements.lowerCaseAlpha = this.generateLowerCaseAlpha() : requirements.lowerCaseAlpha = false;
+    options.specialCharacter ? requirements.specialCharacter = this.generateSpecialCharacter() : requirements.specialCharacter = false;
+    options.number ? requirements.number = this.generateRandomNumber() : requirements.number = false;
 
     _.forEach(requirements, (value, key) => {
+      if(value === false) return;
       let requirementMet = false;
       while (!requirementMet) {
-        const rand = this.generateRandomNumber(passwordLength);
+        debug('Generate index to place requirement. Length of password: ', options.exactLength);
+        const rand = this.generateRandomNumber(options.exactLength); // position between 0 and length of password - 1
+        debug('Index generated: ', rand);
         if (password[rand] === undefined) {
           password[rand] = value;
           requirementMet = true;
@@ -58,9 +75,15 @@ class PasswordGenerator {
       }
     });
 
-    for(let i = 0 ; i < passwordLength; i++) {
+    for(let i = 0 ; i < options.exactLength; i++) {
+      const possibleCharacters = [];
+      if(options.upperCaseAlpha) possibleCharacters.push(this.generateUpperCaseAlpha());
+      if(options.lowerCaseAlpha) possibleCharacters.push(this.generateLowerCaseAlpha());
+      if(options.specialCharacter) possibleCharacters.push(this.generateSpecialCharacter());
+      if(options.number) possibleCharacters.push(this.generateRandomNumber());
+
     	if(password[i] === undefined) {
-      	password[i] = functionArray[this.generateRandomNumber(functionArray.length)]();
+      	password[i] = possibleCharacters[this.generateRandomNumber(possibleCharacters.length)];
       }
     }
 
--- test/test.js
+++ test/test.js
@@ -31,6 +31,21 @@ describe('Unit Test Helper Functions', () => {
       assert.isBelow(randomNumber, 2, randomNumber + ' is less than 2');
       done();
     });
+
+    it('number generated should be between 1 (inclusive) and 2 (exclusive) when 2 and 1 are passed in', (done) => {
+      const randomNumber = passwordGenerator.generateRandomNumber(2,1);
+      assert.isAtLeast(randomNumber, 1, randomNumber + ' is greater than or equal to 1');
+      assert.isBelow(randomNumber, 2, randomNumber + ' is less than 2');
+      done();
+    });
+
+    it('number generated should be between 100 (inclusive) and 256 (exclusive) when 256 and 100 are passed in', (done) => {
+      const randomNumber = passwordGenerator.generateRandomNumber(256,100);
+      assert.isAtLeast(randomNumber, 100, randomNumber + ' is greater than or equal to 100');
+      assert.isBelow(randomNumber, 256, randomNumber + ' is less than 256');
+      done();
+    });
+
   });
 
   describe('generateSpecialCharacter()', () => {
@@ -71,7 +86,182 @@ describe('Unit Test Helper Functions', () => {
 });
 
 describe('Unit Test generatePassword()', () => {
-  describe('Test default password generation', () => {
-    
+  describe('Test each configurable option for password generation', () => {
+    const passwordGenerator = new PasswordGenerator();
+    const upperCaseAlphas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const lowerCaseAlphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '_', '-', '=', '}', '{', '[', ']', '|', ':', ';', '\"', '/', '?', '.', '>', '<', ',', '`', '~', '\''];
+    const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+
+    it('upperCaseAlpha : true', (done) => {
+      const options = {
+        upperCaseAlpha: true
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < upperCaseAlphas.length; i++) {
+        if(password.includes(upperCaseAlphas[i])) break;
+        if(i === upperCaseAlphas.length - 1) throw new Error('Password ' + password + ' does not contain an upper case alpha character');
+      }
+      done();
+    });
+
+    it('upperCaseAlpha : false', (done) => {
+      const options = {
+        upperCaseAlpha: false
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < upperCaseAlphas.length; i++) {
+        if(password.includes(upperCaseAlphas[i])) throw new Error('Password ' + password + ' contains an upper case alpha character when it should not');
+      }
+      done();
+    });
+
+    it('lowerCaseAlpha : true', (done) => {
+      const options = {
+        lowerCaseAlpha: true
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < lowerCaseAlphas.length; i++) {
+        if(password.includes(lowerCaseAlphas[i])) break;
+        if(i === lowerCaseAlphas.length - 1) throw new Error('Password ' + password + ' does not contain a lower case alpha character');
+      }
+      done();
+    });
+
+    it('lowerCaseAlpha : false', (done) => {
+      const options = {
+        lowerCaseAlpha: false
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < lowerCaseAlphas.length; i++) {
+        if(password.includes(lowerCaseAlphas[i])) throw new Error('Password ' + password + ' contains an lower case alpha character when it should not');
+      }
+      done();
+    });
+
+    it('specialCharacter : true', (done) => {
+      const options = {
+        specialCharacter: true
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < specialCharacters.length; i++) {
+        if(password.includes(specialCharacters[i])) break;
+        if(i === specialCharacters.length - 1) throw new Error('Password ' + password + ' does not contain a special character');
+      }
+      done();
+    });
+
+    it('specialCharacter : false', (done) => {
+      const options = {
+        specialCharacter: false
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < specialCharacters.length; i++) {
+        if(password.includes(specialCharacters[i])) throw new Error('Password ' + password + ' contains a special character when it should not');
+      }
+      done();
+    });
+
+    it('number : true', (done) => {
+      const options = {
+        number: true
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < numbers.length; i++) {
+        if(password.includes(numbers[i])) break;
+        if(i === numbers.length - 1) throw new Error('Password ' + password + ' does not contain a number');
+      }
+      done();
+    });
+
+    it('number : false', (done) => {
+      const options = {
+        number: false
+      };
+      const password = passwordGenerator.generatePassword(options);
+      for(let i = 0; i < numbers.length; i++) {
+        if(password.includes(numbers[i])) throw new Error('Password ' + password + ' contains a number when it should not');
+      }
+      done();
+    });
+
+    it('minimumLength : 24', (done) => {
+      const options = {
+        minimumLength: 24
+      };
+      const password = passwordGenerator.generatePassword(options);
+      expect(password.length).to.be.gte(options.minimumLength);
+      done();
+    });
+
+    it('maximumLength : 9', (done) => {
+      const options = {
+        maximumLength: 9
+      };
+      const password = passwordGenerator.generatePassword(options);
+      expect(password.length).to.be.gte(8).and.to.be.lte(options.maximumLength); // 8 is default minimumLength
+      done();
+    });
+
+    it('minimumLength : 4, maximumLength : 7', (done) => {
+      const options = {
+        minimumLength: 4,
+        maximumLength: 7
+      };
+      const password = passwordGenerator.generatePassword(options);
+      expect(password.length).to.be.gte(options.minimumLength).and.to.be.lte(options.maximumLength);
+      done();
+    });
+
+    it('exactLength : 16', (done) => {
+      const options = {
+        exactLength: 16
+      };
+      const password = passwordGenerator.generatePassword(options);
+      expect(password.length).to.equal(options.exactLength);
+      done();
+    });
+  });
+
+  describe('Test all configurable options at once', () => {
+    const passwordGenerator = new PasswordGenerator();
+    const upperCaseAlphas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const lowerCaseAlphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '_', '-', '=', '}', '{', '[', ']', '|', ':', ';', '\"', '/', '?', '.', '>', '<', ',', '`', '~', '\''];
+    const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+    it('minimumLength: 10, maximumLength: 18, number: true, specialCharacter: true, upperCaseAlpha: true, lowerCaseAlpha: true', (done) => {
+      const options = {
+        minimumLength: 10,
+        maximumLength: 18,
+        number: true,
+        specialCharacter: true,
+        upperCaseAlpha: true,
+        lowerCaseAlpha: true
+      };
+      const password = passwordGenerator.generatePassword(options);
+      expect(password.length).to.be.gte(options.minimumLength).and.to.be.lte(options.maximumLength);
+
+      for(let i = 0; i < numbers.length; i++) {
+        if(password.includes(numbers[i])) break;
+        if(i === numbers.length - 1) throw new Error('Password ' + password + ' does not contain a number');
+      }
+
+      for(let i = 0; i < specialCharacters.length; i++) {
+        if(password.includes(specialCharacters[i])) break;
+        if(i === specialCharacters.length - 1) throw new Error('Password ' + password + ' does not contain a special character');
+      }
+
+      for(let i = 0; i < upperCaseAlphas.length; i++) {
+        if(password.includes(upperCaseAlphas[i])) break;
+        if(i === upperCaseAlphas.length - 1) throw new Error('Password ' + password + ' does not contain an upper case alpha character');
+      }
+
+      for(let i = 0; i < lowerCaseAlphas.length; i++) {
+        if(password.includes(lowerCaseAlphas[i])) break;
+        if(i === lowerCaseAlphas.length - 1) throw new Error('Password ' + password + ' does not contain a lower case alpha character');
+      }
+
+      done();
+    });
   });
 });
